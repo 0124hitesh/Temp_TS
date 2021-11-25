5ddb3d5329c7f73a31c3eae19c89389a21f2ae15
Hitesh Sharma <44960249+0124hitesh@users.noreply.github.com>
2021-11-25 15:10:16 +0530

    Bitmap Font, Masking

--- pixi/src/bitmapFont.ts
+++ pixi/src/bitmapFont.ts
@@ -1,6 +1,10 @@
-import {Application, BitmapText   } from "pixi.js"
+import {Application, BitmapText, Graphics} from "pixi.js"
 
-const app = new Application({ backgroundColor: 0x1099bb });
+const app = new Application({ 
+    backgroundColor: 0x1099bb,
+    width: 1200,
+    height: 600
+ });
 document.body.appendChild(app.view);
 
 app.loader
@@ -8,12 +12,51 @@ app.loader
     .load(onAssetsLoaded);
 
 function onAssetsLoaded() {
-    const bitmapFontText = new BitmapText('bitmap fonts are supported!\nWoo yay!', { 
-        fontName: 'Desyrel', fontSize: 55, align: 'left' 
+    const bfm = new BitmapText('Bitmap Fontttttttttttttttttt!', { 
+        fontName: 'Desyrel', 
+        fontSize: 55, 
+        align: 'left' 
     });
 
-    bitmapFontText.x = 50;
-    bitmapFontText.y = 200;
+    bfm.x = 50;
+    bfm.y = 200;
 
-    app.stage.addChild(bitmapFontText);
+    app.stage.addChild(bfm);
+
+    const g = new Graphics();
+    g.beginFill(0xffffff, 1)
+    g.drawRect(50, 50, 200, 200)
+    g.endFill();
+    g.x = 50
+    g.y = 50
+    app.stage.addChild(g)
+
+    const cir = new Graphics();
+    cir.beginFill(0xDE3249, 1);
+    cir.drawCircle(300, 300, 50);
+    cir.endFill();
+    app.stage.addChild(cir)
+
+    app.stage.interactive = true
+    // Interaction --  https://pixijs.io/examples/#/interaction/interactivity.js
+
+    app.stage.on("pointerdown", (e: any) => {
+        app.stage.on("pointermove", showOnMove)
+        bfm.mask = g;
+        cir.mask = g
+        showOnMove(e)
+    })
+
+    function showOnMove(e: any){
+        g.x = e.data.global.x - 100;
+        g.y = e.data.global.y - 100;
+    }
+
+    // app.stage.on("pointerup", () => {
+    //     app.stage.off('pointermove', showOnMove)
+
+    //     bfm.mask = null;
+    //     g.x = 50
+    //     g.y = 200
+    // })
 }
